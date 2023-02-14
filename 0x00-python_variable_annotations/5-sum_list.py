9f28a84f9eeb7b5939f0047470331b9d2df84d8a
Samuthe <ekatisamuel@gmail.com>
2023-02-14 14:47:10 +0300

    list of float

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,7 @@
 from typing import Callable, Iterator, Union, Optional, List
-image.png
 #!/usr/bin/env python3
 """ Complex types - list of floats """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
